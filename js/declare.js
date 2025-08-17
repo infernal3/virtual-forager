7a9209493b664d690f85d20462c90258e5ed2944
infernal3 <infernal1559@gmail.com>
2025-08-17 14:13:09 -0500

    Update declare.js

--- js/declare.js
+++ js/declare.js
@@ -4,12 +4,13 @@ const NaNCheck = e => isNaN(e) ? "0" : e;
 const CBT = (e, t) => {el(`root_button_${t}`).textContent = e}
 
 const T_DATA = [null,
-    {name: "Oak", xp: 1, completeXPLow: 8, completeXPHigh: 12, toughness: 0, baseLogs: 12, req: 0},
-    {name: "Birch", xp: 4, completeXPLow: 40, completeXPHigh: 55, toughness: 2, baseLogs: 17, req: 10},
-    {name: "Spruce", xp: 9, completeXPLow: 130, completeXPHigh: 150, toughness: 5, baseLogs: 44, req: 20},
-    {name: "Palm", xp: 30, completeXPLow: 400, completeXPHigh: 450, toughness: 19, baseLogs: 20, req: 25},
-    {name: "Mangrove", xp: 200, completeXPLow: 6000, completeXPHigh: 8000, toughness: 85, baseLogs: 38, req: 50},
-    {name: "Pine", xp: 3750, completeXPLow: 80000, completeXPHigh: 100000, toughness: 700, baseLogs: 60, req: 100}
+    {name: "Oak", xp: 1, completeXPLow: 8, completeXPHigh: 12, toughness: 0, baseLogs: 12, req: 0, cute_name: "Oak Forest"},
+    {name: "Birch", xp: 4, completeXPLow: 40, completeXPHigh: 55, toughness: 2, baseLogs: 17, req: 10, cute_name: "Birch Forest"},
+    {name: "Spruce", xp: 9, completeXPLow: 130, completeXPHigh: 150, toughness: 5, baseLogs: 44, req: 20, cute_name: "Spruce Woods"},
+    {name: "Palm", xp: 30, completeXPLow: 400, completeXPHigh: 450, toughness: 19, baseLogs: 20, req: 25, cute_name: "Beachside"},
+    {name: "Crimson", xp: 200, completeXPLow: 6000, completeXPHigh: 8000, toughness: 80, baseLogs: 38, req: 50, cute_name: "The Nether"},
+    {name: "Pine", xp: 3750, completeXPLow: 80000, completeXPHigh: 100000, toughness: 470, baseLogs: 60, req: 100, cute_name: "Pinerise Peak"},
+    {name: "Mangrove", xp: 540, completeXPLow: 11000, completeXPHigh: 13500, toughness: 50, baseLogs: 140, req: 100, cute_name: "Mangrove Marsh"},
 ]
 const TOOLS = [null,
     {name: "Wooden Axe", lore: "It's made of Oak Wood.", sweep: 1, cost: 0, speed: 3500},
@@ -20,11 +21,11 @@ const TOOLS = [null,
     {name: "Chainsaw", lore: "Cuts down a huge amount of logs.", sweep: 19, cost: 10000, speed: 300},
     {name: "Polished Chainsaw", lore: "It's slightly better than an ordinary Chainsaw.", sweep: 21, cost: 50000, speed: 180},
     {name: "Treecapacitator", lore: "A forceful Axe that cuts many logs at once.", sweep: 35, cost: 250000, speed: 50},
-    {name: "Lava Axe", lore: "Cuts trees down with the power of flames.", sweep: 45, cost: 1000000, speed: 40},
+    {name: "Lava Axe", lore: "Cuts trees down with the power of flames.", sweep: 85, cost: 1000000, speed: 40},
     {name: "Molten Axe", lore: "It's hotter than Lava. It's molten hot!", sweep: 95, cost: 10000000, speed: 33},
 ]
 const S_DATA = {logs: {
-    Oak: 1, Birch: 10, Spruce: 50, Palm: 275, Mangrove: 900, Pine: 8000
+    Oak: 1, Birch: 10, Spruce: 50, Palm: 275, Crimson: 900, Pine: 2000, Mangrove: 14000
 }, cores: {
-    Oak: 3, Birch: 25, Spruce: 200, Palm: 1500, Mangrove: 10000, Pine: 150000
+    Oak: 3, Birch: 25, Spruce: 200, Palm: 1500, Crimson: 10000, Pine: 25000, Mangrove: 610000
 }}
