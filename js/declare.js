009570d5d51b0aa33d78866aaafefda8ee1ab7b7
infernal3 <infernal1559@gmail.com>
2026-05-11 21:56:32 -0500

    Update declare.js

--- js/declare.js
+++ js/declare.js
@@ -26,10 +26,10 @@ const T_DATA = [null,
     {name: "Birch",   xp: 4,    completeXPLow: 40,   completeXPHigh: 55,   toughness: 2,  baseLogs: 17, req: 10, height: 411, cute_name: "Birch Forest"},
     {name: "Spruce",  xp: 9,    completeXPLow: 130,  completeXPHigh: 150,  toughness: 5,  baseLogs: 44, req: 20, height: 416, cute_name: "Spruce Woods"},
     {name: "Palm",    xp: 30,   completeXPLow: 600,  completeXPHigh: 675,  toughness: 19, baseLogs: 20, req: 25, height: 331, cute_name: "Palm Beach"},
-    {name: "Crimson", xp: 240,  completeXPLow: 6000, completeXPHigh: 8000, toughness: 72, baseLogs: 38, req: 50, height: 381, cute_name: "The Nether"},
+    {name: "Crimson", xp: 240,  completeXPLow: 6000, completeXPHigh: 8000, toughness: 72, baseLogs: 38, req: 50, height: 381, cute_name: "The Volcano"},
     {name: "Willow",  xp: 155,  completeXPLow: 2000, completeXPHigh: 2600, toughness: 32, baseLogs: 15, req: 75, height: 194, cute_name: "Willow Wilds"},
-    {name: "Pine",    xp: 27500,completeXPLow:890000,completeXPHigh:999999,toughness: 470,baseLogs: 90, req: 100,height: 471, cute_name: "Pinerise Peak"},
-    {name: "Mangrove",xp: 486,  completeXPLow: 11000,completeXPHigh: 13500,toughness: 50, baseLogs: 94, req: 125,height: 384, cute_name: "Mangrove Marsh"},
+    {name: "Pine",    xp: 27500,completeXPLow:890000,completeXPHigh:999999,toughness: 240,baseLogs:170, req: 100,height: 471, cute_name: "Pinerise Peak"},
+    {name: "Mangrove",xp: 486,  completeXPLow: 11000,completeXPHigh: 13500,toughness: 55, baseLogs: 94, req: 125,height: 384, cute_name: "Mangrove Marsh"},
 ]
 const TOOLS = [null,
     {name: "Wooden Axe", lore: "It's made of Oak Wood.",                          sweep: 1,   req: 0,  cost: 0, speed: 3500},
@@ -43,26 +43,41 @@ const TOOLS = [null,
     {name: "Lava Axe", lore: "Cuts trees down with the power of flames.",         sweep: 85,  req: 0,  cost: 1200000, speed: 45},
     {name: "Molten Axe", lore: "It's hotter than Lava. It's molten hot!",         sweep: 160, req: 50, cost: 7500000, speed: 40},
     {name: "Diamond Axe", lore: "Made from pure diamonds.",                       sweep: 60,  req: 50, cost: 24000000, speed: 16},
-    {name: "Mythical Axe", lore: "Only heard of in obscure myths, until now.",    sweep: 250, req: 100,cost: 180000000, speed: 37},
-    {name: "Molten Chainsaw", lore: "Molten hot mechanized glory.",               sweep: 480, req: 100,cost: 640000000, speed: 35},
+    {name: "Mythical Axe", lore: "Cuts trees down with the power of... myths?",   sweep: 250, req: 100,cost: 180000000, speed: 37},
+    {name: "Molten Chainsaw", lore: "Cuts trees down with fire. Now a chainsaw.", sweep: 480, req: 100,cost: 640000000, speed: 35},
     {name: "Infinity Axe", lore: "???",                                           sweep: 999, req: 100,cost: 2147483647, speed: 32},
 ]
 const S_DATA = {logs: {
-    Oak: 1, Birch: 10, Spruce: 50, Palm: 275, Crimson: 900, Willow: 1700, Pine: 4000,  Mangrove: 10500
+    Oak: 1, 
+    Birch: 10, 
+    Spruce: 50, 
+    Palm: 275, 
+    Crimson: 900, 
+    Willow: 1700, 
+    Pine: 4000,  
+    Mangrove: 10500
 }, cores: {
-    Oak: 3, Birch: 25, Spruce: 200,Palm: 1500,Crimson: 8500,Willow: 4000, Pine: 250000,Mangrove: 366000
+    Oak: 3, 
+    Birch: 25, 
+    Spruce: 200,
+    Palm: 1500,
+    Crimson: 8500,
+    Willow: 4000, 
+    Pine: 250000,
+    Mangrove: 366000
 }};
 const SHOPS = [null,
     {name: "Tools Shop", lore: "The place to buy new tools such as an axe or chainsaw."},
     {name: "Upgrades Shop", lore: "Unlock permanent upgrades for your character."},
     {name: "Special Gallery", lore: "View the boosts that your Special Items are giving."},
+  //{name: "Pure Upgrade Shop", lore: "Use Pure Cores to unlock more permanent upgrades."},
 ]
 const SPECIAL = {
     "Pure Core": {lore: "The quality is so pure that it's priceless. Can't sell it."},
     "Pure Sweep Core": {lore: "Your tools gain +1% sweep."},
     "Pure Wisdom Core": {lore: "You gain +1% more XP."},
     "Pure Tree Core": {lore: "Trees give +1% more Cores."},
-    "Tree the Fish": {lore: "An extremely rare drop, where did you find this? (0.001% chance)"}
+    "Tree the Fish": {lore: "An extremely rare drop, where did you find this? (0.001% chance)"},
 }
 const UPG = [null,
     {name: "Sweep Buff", lore: "Increase the Sweep of all tools by +5%.",       max:20, tiers: [500, 750, 1000, 1250, 1500, 2000, 2500, 3000, 3500, 4000, 5000, 7500, 10000, 15000, 20000, 50000, 200000, 1000000, 5000000, 20000000, Infinity]},
@@ -71,4 +86,10 @@ const UPG = [null,
     {name: "Core Specialist", lore: "All trees drop +20% more Cores.",          max: 5, tiers: [20000, 75000, 250000, 1000000, 20000000, Infinity]},
     {name: "Efficiency", lore: "Increase the cut speed of all tools by +10%.",  max: 5, tiers: [10, 2000, 100000, 2000000, 20000000, Infinity]},
     {name: "Tree Gifts", lore: "Rare rewards from trees are +100% more common.",max: 1, tiers: [1000000000, Infinity]},
+]
+const UPG2 = [null,
+    {name: "Pure Sweep Buff", lore: "Increase the sweep of all tools by +10%.",       max:5, tiers: [1, 5, 10, 25, 100, Infinity]},
+    {name: "Pure Efficiency", lore: "Increase the cut speed of all tools by +10%.",   max:5, tiers: [1, 5, 10, 25, 100, Infinity]},
+    {name: "Pure Tree Gifts", lore: "Rare rewards from trees are +20% more common.",  max:5, tiers: [1, 5, 10, 25, 100, Infinity]},
+    {name: "Unlock Prestige", lore: "Purchase this upgrade to find out what it does!",max:1, tiers: [10000, Infinity]},
 ]
